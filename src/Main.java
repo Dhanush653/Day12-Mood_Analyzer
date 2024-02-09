b41124c2e53a54f832a0d2a5014775e893a3632d
Dhanush Kumar <dk652003@gmail.com>
2024-02-09 15:46:10 +0530

    Added Exception Handling for Invalid Mood

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,14 @@ import com.bridgelabz.moodanalyzer.Moodanalyzer;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Moodanalyzer moodAnalyzer = new Moodanalyzer();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your Message: ");
-        String message = scanner.nextLine();
-        System.out.println(moodAnalyzer.analyzeMood(message));
 
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter your Message: ");
+//        String message = scanner.nextLine();
+        String message = null;
 
-
+        //Refactored
+        Moodanalyzer moodAnalyzer = new Moodanalyzer(message);
+        moodAnalyzer.analyzeMood();
     }
 }
--- src/com/bridgelabz/moodanalyzer/Moodanalyzer.java
+++ src/com/bridgelabz/moodanalyzer/Moodanalyzer.java
@@ -1,13 +1,23 @@
 package com.bridgelabz.moodanalyzer;
 
 public class Moodanalyzer {
-    public String analyzeMood(String message){
-        if(message.contains("sad") || message.contains("Sad")){
-            return "SAD";
-        }
-        else{
-            return "HAPPY";
-        }
+    String message;
+
+    public Moodanalyzer(String message) {
+        this.message = message;
     }
 
+    public void analyzeMood() {
+        try{
+            if (message.contains("sad") || message.contains("Sad") || message.contains("SAD")) {
+                System.out.println("Sad");
+            }
+            if (!message.contains("sad") || !message.contains("Sad") || !message.contains("SAD")){
+                System.out.println("HAPPY");
+            }
+        }
+        catch (NullPointerException e){
+            System.out.println("happy (You have enter Null String)");
+        }
+    }
 }
