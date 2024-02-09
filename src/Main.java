c10b5cf36be7220d39029fb84d1540f3ae89c86d
Dhanush Kumar <dk652003@gmail.com>
2024-02-09 12:25:16 +0530

    Added analyzeMood Method to analyze the mood

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
-
+import com.bridgelabz.moodanalyzer.Moodanalyzer;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Moodanalyzer moodAnalyzer = new Moodanalyzer();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your Message: ");
+        String message = scanner.nextLine();
+        System.out.println(moodAnalyzer.analyzeMood(message));
+
+
+
     }
 }
--- src/com/bridgelabz/moodanalyzer/Moodanalyzer.java
+++ src/com/bridgelabz/moodanalyzer/Moodanalyzer.java
@@ -1,5 +1,13 @@
 package com.bridgelabz.moodanalyzer;
 
 public class Moodanalyzer {
+    public String analyzeMood(String message){
+        if(message.contains("sad") || message.contains("Sad")){
+            return "SAD";
+        }
+        else{
+            return "HAPPY";
+        }
+    }
 
 }
